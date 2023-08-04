6a34762705e9c3d3c02057c7f7822c1c86292a86
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-08-04 23:26:03 +0100

    2

--- src/components/HeaderSocials/HeaderSocials.jsx
+++ src/components/HeaderSocials/HeaderSocials.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { BsLinkedin } from 'react-icons/bs';
 import { FaGithub } from 'react-icons/fa';
 import { FaTelegramPlane } from "react-icons/fa";
+import './headerSocials.css';
 
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
       <a href="https://www.linkedin.com/in/oleksandr-tkachenko-developer/" target="_blank" rel="noreferrer noopener">
-        <BsLinkedin />
+        <BsLinkedin className='header_icon'/>
       </a>
       <a href="https://github.com/AlexTkachenkoFullstack" target="_blank" rel="noreferrer noopener">
-        <FaGithub />
+        <FaGithub className='header_icon'/>
       </a>
       <a href="http://t.me/alex_goltf" target="_blank" rel="noreferrer noopener">
-        <FaTelegramPlane />
+        <FaTelegramPlane className='header_icon'/>
       </a>
     </div>
   );
