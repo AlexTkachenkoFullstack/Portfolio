5e12dfa1230345da3ebdf87b6ef9b9b745c8eef8
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-11-11 23:21:06 +0000

    fix about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -19,7 +19,13 @@ const About = () => {
           </div>
 
           <p>
-          I am a Junior fullstack Developer with a strong grasp of HTML, CSS, JavaScript, TypeScript, React, Node.js, and React Native. I have experience working as a team lead on collaborative projects and specialize in crafting visually appealing and user-friendly web applications, while also writing efficient JavaScript code. My enthusiasm for innovation drives me to continually expand my skill set and explore emerging technologies. I am a fast learner with keen attention to detail and am eager to join a team where I can further develop both personally and professionally, making meaningful contributions to project success.
+            I am a Front-end developer with advanced skills in HTML, CSS,
+            JavaScript, TypeScript, React, Node.js, and React Native. My
+            expertise lies in creating visually appealing and user-friendly web
+            applications, as well as writing efficient JavaScript code. I have
+            experience working on collaborative commercial projects and
+            volunteering on projects from ideation to implementation as a
+            Frontend Developer.
           </p>
 
           <a href="#contact" className="btn btn-primary">
